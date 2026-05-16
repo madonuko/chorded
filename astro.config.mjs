2ee5bf396f0189c15db7b4473dc9a709aebc8bd1
madonuko <madonuko@outlook.com>
2026-05-16 17:19:33 +0800

    feat: make things static, correct base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/chorded/',
+  output: 'static',
   vite: {
     plugins: [tailwindcss()]
   },
